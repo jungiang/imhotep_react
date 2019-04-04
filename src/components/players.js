b5701360c89af90112689c3f5e8e5a074aa61197
Jason <jasonyata@gmail.com>
2019-04-04 10:09:04 -0700

    added fab, must add click handler and functunality

--- src/components/players.js
+++ src/components/players.js
@@ -20,17 +20,26 @@ class Players extends Component {
     handleBlockRecharge = () => {
         const { stones, color } = this.state.stones;
 
-        if (stones.length < 3){
-            const newStones = [{color}, {color}, {color}];
+        if (stones.length < 3) {
+            const newStones = [{ color }, { color }, { color }];
             this.setState({
                 stones: stones.push(...newStones)
             });
         }
-        if (stones.length === 3){
-            const newStones = [{color}, {color}];
+        if (stones.length === 3) {
+            const newStones = [{ color }, { color }];
             this.setState({
                 stones: stones.push(...newStones)
             });
+        }
+        if (stones.length === 4) {
+            const newStones = [{ color }];
+            this.setState({
+                stones: stones.push(newStones)
+            });
+        }
+        if (stones.length === 5) {
+            return
         }
     }
 
@@ -40,20 +49,27 @@ class Players extends Component {
 
         return (
             <div className="row">
-            <div className="col s4 m3">
-              <div className="card orange darken-1">
-                <div className="card-content white-text">
-                  <span className="card-title">{name}</span>
-                    <p>Stones: {stoneCount}</p>
-                    <p>{score}</p>
-                    <img src={icon} alt="pug"/>
+                <div className="col s4 m3">
+                    <div className="card orange darken-1">
+                        <div className="card-content white-text">
+                            <span className="card-title">{name}</span>
+                            <p>Score: {score}</p>
+                            <img src={icon} alt="pug" />
+                            <p>Stones: {stoneCount}</p>
+                            <div className="fixed-action-btn">
+                                <a className="btn-floating btn-med red">
+                                    <i className="large material-icons">add_circle</i>
+                                </a>
+                                <ul>
+                                    <li onClick={this.handleBlockRecharge}><a className="btn-floating red"><i className="material-icons">check</i></a></li>
+                                    <li><a className="btn-floating blue darken-1"><i className="material-icons">close</i></a></li>
+                                </ul>
+                            </div>
+                            <p onClick={this.displayCards}>Cards</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="card-action">
-                    <p onClick={this.displayCards}>Cards</p>
-                </div>
-              </div>
             </div>
-          </div>
         )
     }
 }
