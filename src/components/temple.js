65200726f59847c4ce54106cf748c0104632f4a4
Davideuijinlee <Davideuijinlee@gmail.com>
2019-04-07 03:00:12 -0700

    added point feature for Temple

--- src/components/temple.js
+++ src/components/temple.js
@@ -3,18 +3,19 @@ import React, {Component} from 'react';
 
 class Temple extends Component {
         state={
-            'stones': [], //will have length of 5
+            'stones': [], 
             'stoneReplacementPos': 0,
             'docked': false,
-            'players':{},
-            'color': []
+            'playersStoneCount':{},
+            'color': [],
+            'templePoints': {}
         };
         
     addStonesToArray=()=>{
+        // debugger;
         const {stones} = this.state;
         let {stoneReplacementPos} = this.state;
-        const shipBlocks = ['red', 'blue', 'red'] //dummydata
-        // this.stoneColor = this.currentStone.css('background-color'); //currentlu jquery. how will we change set in react?
+        const shipBlocks = ['red', 'black', 'green'] //dummydata
         for( let index = 0; index < shipBlocks.length; index++){
             if(this.state.stones.length < 5){
                 const currentStonesArray = stones;
@@ -26,19 +27,16 @@ class Temple extends Component {
             const newStonesArray = stones;
             if(stoneReplacementPos < 5){
                 newStonesArray[stoneReplacementPos] = shipBlocks[index];
-                this.setState({
-                    stones: newStonesArray,
-                    stoneReplacementPos: ++stoneReplacementPos
-                })
             } else{
                 newStonesArray[0] = shipBlocks[index];
-                this.setState({
-                    stones: newStonesArray,
-                    stoneReplacementPos: 0
-                })
+                stoneReplacementPos = 0;
             }
+            this.setState({
+                'stones': newStonesArray,
+                stoneReplacementPos: ++stoneReplacementPos
+            })
         }
-        this.colorBlocks(shipBlocks[index])
+        // this.colorBlocks(shipBlocks[index])
         this.addPlayerCounter(shipBlocks[index]);
 
         }
@@ -46,29 +44,40 @@ class Temple extends Component {
 
 
     addPlayerCounter(color){
-        debugger;
-        const {players} = this.state;
-        if(!(players[color])){
-            players[color] = 1
-            this.setState({
-                players
-            })
-            console.log('new player', this.state.players);
+        const {playersStoneCount} = this.state;
+        if(!(playersStoneCount[color])){
+            playersStoneCount[color] = 1
         } else{
-            players[color] += 1;
-            this.setState({
-                players
-            })
-            console.log('existing player', this.state.players)
+            playersStoneCount[color] += 1;
         }
+        this.setState({
+            playersStoneCount
+        });
     }
 
-    colorBlocks(color){
-        const stoneElement = <div className="blockTest" style='background-color:{$color}'></div>
+    calculateTemplePoints=()=>{
+        const {stones} = this.state;
+        let {templePoints} = this.state;
+        for(let stoneIndex = 0; stoneIndex < stones.length; stoneIndex++){
+            if(!(templePoints[stones[stoneIndex]])){
+                templePoints[stones[stoneIndex]] = 1
+            } else{
+                templePoints[stones[stoneIndex]]++
+            }
+        }
+        this.setState({
+            templePoints
+        });
+    }
+
+    createTempleBlock(){
+
     }
 
     render(){
-        console.log(this.state.stones)
+        console.log('stones', this.state.stones);
+        console.log('stonecount', this.state.playersStoneCount);
+        console.log('templePoints', this.state.templePoints);
         return(            
         <div className="temple">
             <div className="blockContainer">
@@ -78,6 +87,7 @@ class Temple extends Component {
                 <div className="blockTest"></div>
                 <div className="blockTest"></div>  
                 <button onClick={this.addStonesToArray} >add stone</button>
+                <button onClick={this.calculateTemplePoints}>Calc Pts End of Round</button>
             </div>
         </div>
         )
