51150be65ba727723b5272566e8ffa4cdfdac50c
Davideuijinlee <Davideuijinlee@gmail.com>
2019-04-06 23:15:21 -0700

    temple counter counts properlyincrements if color exists creates new color key if it does not

--- src/components/temple.js
+++ src/components/temple.js
@@ -3,20 +3,17 @@ import React, {Component} from 'react';
 
 class Temple extends Component {
         state={
-            stones: ['black', 'black', 'black', 'black'], //will have length of 5
-            stoneReplacementPos: 0,
-            docked: false,
-            players:{
-                'player1StoneCount': 0, //player count will vary using 2 as dummy data
-                'player2StoneCount': 0,
-            }
+            'stones': [], //will have length of 5
+            'stoneReplacementPos': 0,
+            'docked': false,
+            'players':{},
+            'color': []
         };
         
-
     addStonesToArray=()=>{
         const {stones} = this.state;
         let {stoneReplacementPos} = this.state;
-        const shipBlocks = ['red', 'blue', 'green'] //dummydata
+        const shipBlocks = ['red', 'blue', 'red'] //dummydata
         // this.stoneColor = this.currentStone.css('background-color'); //currentlu jquery. how will we change set in react?
         for( let index = 0; index < shipBlocks.length; index++){
             if(this.state.stones.length < 5){
@@ -41,18 +38,33 @@ class Temple extends Component {
                 })
             }
         }
+        this.colorBlocks(shipBlocks[index])
+        this.addPlayerCounter(shipBlocks[index]);
+
         }
     }
 
 
-    calculate(){
-        const {stones} = this.state;
-        for (let index = 0; index < stones.length; index++){
-            const lastStoneInArray = stones[index][stones[index].length-1];
-            if(lastStoneInArray === 'black'){ //color
-                blackpoint++; //add point
-            }
+    addPlayerCounter(color){
+        debugger;
+        const {players} = this.state;
+        if(!(players[color])){
+            players[color] = 1
+            this.setState({
+                players
+            })
+            console.log('new player', this.state.players);
+        } else{
+            players[color] += 1;
+            this.setState({
+                players
+            })
+            console.log('existing player', this.state.players)
         }
+    }
+
+    colorBlocks(color){
+        const stoneElement = <div className="blockTest" style='background-color:{$color}'></div>
     }
 
     render(){
@@ -60,13 +72,12 @@ class Temple extends Component {
         return(            
         <div className="temple">
             <div className="blockContainer">
-                <div className=" red blockTest"></div>
+                <div className="blockTest"></div>
                 <div className="blockTest"></div>
                 <div className="blockTest"></div>
                 <div className="blockTest"></div>
                 <div className="blockTest"></div>  
                 <button onClick={this.addStonesToArray} >add stone</button>
-                <button onClick={this.calculate}>calculate</button>
             </div>
         </div>
         )
